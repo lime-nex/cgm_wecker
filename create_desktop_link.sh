e1c7b19d602f207259f2bd1f6f2258f4d285d5a2
lime-nex <lennart.heinlein@gmx.de>
2025-05-27 19:25:57 +0200

    Add files via upload

--- create_desktop_link.sh
+++ create_desktop_link.sh
@@ -3,13 +3,14 @@
 DESKTOP_PATH="$HOME/Desktop"
 SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 EXEC_PATH="$SCRIPT_PATH/start_interface.sh"
+ICON_PATH="$SCRIPT_PATH/icons/icon1.png"
 
 cat > "$DESKTOP_PATH/cgm_wecker.desktop" <<EOF
 [Desktop Entry]
 Type=Application
 Name=CGM Wecker starten
 Exec=bash -c "$EXEC_PATH"
-Icon=utilities-terminal
+Icon=$ICON_PATH
 Terminal=true
 EOF
 
