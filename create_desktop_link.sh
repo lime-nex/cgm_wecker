c2e3458d8fccc8f6605f83b4c45bc39f606ba405
lime-nex <lennart.heinlein@gmx.de>
2025-05-31 18:44:38 +0200

    Add files via upload

--- create_desktop_link.sh
+++ create_desktop_link.sh
@@ -5,7 +5,7 @@ SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 EXEC_PATH="$SCRIPT_PATH/start_interface.sh"
 ICON_PATH="$SCRIPT_PATH/icons/icon1.png"
 
-cat > "$DESKTOP_PATH/cgm_wecker.desktop" <<EOF
+cat > /tmp/cgm_wecker.desktop <<EOF
 [Desktop Entry]
 Type=Application
 Name=CGM Wecker starten
@@ -14,8 +14,9 @@ Icon=$ICON_PATH
 Terminal=true
 EOF
 
-chmod +x "$DESKTOP_PATH/cgm_wecker.desktop"
+sleep 5s
 
-lxpanelctl restart
+chmod +x /tmp/cgm_wecker.desktop
+mv /tmp/cgm_wecker.desktop "$DESKTOP_PATH/cgm_wecker.desktop"
 
 chmod +x start_interface.sh
