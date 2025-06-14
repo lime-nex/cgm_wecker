0c966a9df228fb489b901083776cd23fdcd89732
lime-nex <lennart.heinlein@gmx.de>
2025-06-14 13:46:49 +0200

    Add files via upload

--- create_desktop_link.sh
+++ create_desktop_link.sh
@@ -2,17 +2,30 @@
 
 DESKTOP_PATH="$HOME/Desktop"
 SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-EXEC_PATH="$SCRIPT_PATH/start_interface.sh"
+EXEC_PATH="$SCRIPT_PATH/start_sound.sh"
 ICON_PATH="$SCRIPT_PATH/icons/icon1.png"
 
 cat > /tmp/cgm_wecker.desktop <<EOF
 [Desktop Entry]
 Type=Application
 Name=CGM Wecker starten
-Exec=bash -c "$EXEC_PATH"
+Exec=lxterminal --command="bash --login -c '$EXEC_PATH; exec bash'"
 Icon=$ICON_PATH
 Terminal=true
 EOF
+
+cat << 'EOF' > "$EXEC_PATH"
+#!/bin/bash
+
+SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+EXEC_PATH="$SCRIPT_PATH/start_sound.sh"
+
+export XDG_RUNTIME_DIR=/run/user/$(id -u)
+exec "$SCRIPT_PATH/venv/bin/python3" "$SCRIPT_PATH/Intertface.py"
+EOF
+
+chmod +x "$EXEC_PATH"
+
 
 
 
